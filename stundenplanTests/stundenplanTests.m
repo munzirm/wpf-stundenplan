ab9a2ff60bc62d150d2854953769630679fe39ab
Christoph Jerolimov <jerolimov+git+2012@gmail.com>
2013-01-19 20:09:06 +0100

    Make test green, no real tests yet.

--- stundenplanTests/stundenplanTests.m
+++ stundenplanTests/stundenplanTests.m
@@ -10,23 +10,8 @@
 
 @implementation stundenplanTests
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in stundenplanTests");
+- (void)testExample {
+//	STFail(@"Unit tests are not implemented yet in stundenplanTests");
 }
 
 @end
