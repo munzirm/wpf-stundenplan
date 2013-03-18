2349c2fde8626e11cd50a17cd1de7631a22874b8
Dominik Schilling <dominikschilling+git@gmail.com>
2013-03-18 18:59:30 +0100

    First pass for detail view, see #26

---
+++ stundenplan/ModulEventDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModulEventDetailViewController.h
+//  stundenplan
+//
+//  Created by Dominik Schilling on 18.03.13.
+//  Copyright (c) 2013 FH-K√∂ln. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModulEventDetailViewController : UITableViewController
+
+@end
